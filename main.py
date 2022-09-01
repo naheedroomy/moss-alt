42e0b56fef9b37781ea9461e1a0d9cceccd1c625
Naheed Roomy <46345736+naheedroomy@users.noreply.github.com>
2022-09-01 14:24:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import subprocess
 import time
 import platform, socket, re, uuid, json, psutil, logging
+
+import pythoncom
 import wmi
 from datetime import datetime
 import pyscreenshot as ImageGrab
@@ -10,45 +12,34 @@ import zipfile
 import os
 import shutil
 import time
+import PySimpleGUI as sg
+import threading
 
 count = 1
+status = True
 from mss import mss
 
 import os
 
-# You should change 'test' to your preferred folder.
 datetime_str = str(datetime.now())
 datetime_str_mod = datetime_str.replace(':', '-')
 datetime_str_mod_2 = datetime_str_mod.replace('.', '-')
-mydir = f"C:\\Users\\Administrator\\Desktop\\test\\{datetime_str_mod_2}"
-CHECK_FOLDER = os.path.isdir(mydir)
-
-if not CHECK_FOLDER:
-    os.makedirs(mydir)
-    # print("created folder : ", MYDIR)
 
 
-# def take_screenshot():
-#     global count
-#     datetime_str = datetime.now()
-#     time = datetime_str.time()
-#     time_str = time.strftime("%H:%M:%S")
-#     time_str = time_str.replace(":", "-")
-#     image_name = str(time) + ".png"
-#     screenshot = ImageGrab.grab()
-#     dir_output = f"C:\\Users\\Administrator\\Desktop\\test\\{datetime_str_mod_2}\\"
-#     filepath2 = dir_output + str(count) + ".png"
-#
-#     screenshot.save(filepath2)
-#     count += 1
-#
-#     return filepath2
+def create_folder():
+    global datetime_str_mod_2
+    mydir = f"C:\\Users\\Administrator\\Desktop\\test\\{datetime_str_mod_2}"
+    CHECK_FOLDER = os.path.isdir(mydir)
+
+    if not CHECK_FOLDER:
+        os.makedirs(mydir)
 
 
 
 def take_screenshot2():
     global count
-    final_dir = dir_output = f"C:\\Users\\Administrator\\Desktop\\test\\{datetime_str_mod_2}\\"
+    global datetime_str_mod_2
+    final_dir = f"C:\\Users\\Administrator\\Desktop\\test\\{datetime_str_mod_2}\\"
     final_dir_2 = final_dir + str(count) + ".jpg"
     with mss() as sct:
         sct.shot(mon=-1, output=final_dir_2)
@@ -56,6 +47,8 @@ def take_screenshot2():
 
 
 def get_system_info():
+    pythoncom.CoInitialize()
+    global datetime_str_mod_2
     computer = wmi.WMI()
     gpu_info = computer.Win32_VideoController()[0].name
     info = {}
@@ -78,15 +71,17 @@ def get_system_info():
     except Exception as e:
         logging.exception(e)
 
+
 def get_processes():
+    pythoncom.CoInitialize()
+    global datetime_str_mod_2
     final_dir = f"C:\\Users\\Administrator\\Desktop\\test\\{datetime_str_mod_2}\\"
     final_dir_2 = final_dir + "processes.txt"
     output = os.popen('wmic process get description, processid').read()
     now = datetime.now().strftime("%H:%M:%S")
 
-#     write output to file
+    #     write output to file
     with open(final_dir_2, "a") as f:
-
         f.write("Process snapshot taken at :" + now + "\n")
         f.write(output)
 
@@ -105,17 +100,52 @@ def create_zip_with_password():
     shutil.rmtree(dir_output)
 
 
-def main():
+def schedule_job():
+    global status
+    create_folder()
     get_system_info()
-    schedule.every(25).seconds.do(get_processes)
-    schedule.every(25).seconds.do(take_screenshot2)
+    schedule.every(2).seconds.do(get_processes)
+    schedule.every(2).seconds.do(take_screenshot2)
 
-    while True:
+    while status:
         schedule.run_pending()
         time.sleep(1)
 
 
-#
+def use_threading():
+    pythoncom.CoInitialize()
+    thread1 = threading.Thread(target=schedule_job)
+
+    thread1.start()
+
+
+def stop_job():
+    global status
+    status = False
+
+
+def main():
+    layout = [[sg.Button('Start'), sg.Button('Stop', disabled=True), sg.Exit(disabled=False)]]
+    window = sg.Window('Window Title', layout)
+
+    while True:
+        event, values = window.read(timeout=10)
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == 'Start':
+            window['Start'].update(disabled=True)
+            window['Stop'].update(disabled=False)
+            # disable the exit button
+            window['Exit'].update(disabled=True)
+            use_threading()
+        elif event == 'Stop':
+            stop_job()
+            window['Exit'].update(disabled=False)
+        elif event == '-FUNCTION COMPLETED-':
+            sg.popup('Your function completed!')
+    window.close()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
